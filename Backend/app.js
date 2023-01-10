fc61f443bf6fdb32cc4861ab964916d7fd0ca774
Eleazar Morales Garduño <evil5464@gmail.com>
2023-01-10 17:28:37 -0600

    Se elimina conexion de prueba en app.js

--- Backend/app.js
+++ Backend/app.js
@@ -20,18 +20,5 @@ app.use(bodyParser.json());
 //añadir prefijos a rutas / Cargar rutas
 app.use('/api', article_routers);
 
-//Ruta o método de prueba para el API REST
-/* exp.post("/probando", (req, res) => {
-  var hola = req.body.hola;
-  console.log("Hola Mundo desde la escucha del servidor");
-  return res.status(200).send({
-    curso: "Master en Frameworks",
-    autor: "Victor Robles WEB",
-    url: "victorroblesweb.es",
-    hola
-  });
-}); */
-
-
 //Export MODULO (fichero actual)
 module.exports = app;
